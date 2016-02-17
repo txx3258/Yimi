c5f60426e2551ccd4b3a7226e8c529ed8bd07e30
txx3258 <soft_tangxiaoxian@163.com>
2016-02-17 09:23:07 +0800

    mm

--- src/client.js
+++ src/client.js
@@ -1,8 +1,5 @@
 'use strict';
 
-/*let fs=require('fs');
-let zlib=require('zlib');
-let net=require('net');*/
 let co=require('co');
 
 let config=require('../config.json');
@@ -10,17 +7,6 @@ let myLog4js=require('./myLog4js');
 let logIndex=myLog4js.logIndex,
     logBiz=myLog4js.logBiz,
     logSys=myLog4js.logSys;
-/*
- * 创建TCP客户端，设置IP和PORT
-
-let client=new net.Socket();
-client.setEncoding('utf8');*/
-//连接服务器，如果失败则程序终止
-/*
-client.connect(config.PORT,config.IP,function(){
-
-});*/ 
-
 
 /*
  *业务处理
@@ -42,11 +28,6 @@ let readStrWrap=require('./fileHandler/readStrWrap');
 let sendData=require('./tcpHandler/tcpClient');
 let paths=config.PATHS;
 
-//没有配置文件，程序结束
-if (!paths||paths.length==0){
-  logIndex.info('paths is null.')
-  process.exit(0);
-}
 
 function* bizCode(){
   //读取新增文件信息
@@ -58,10 +39,17 @@ function* bizCode(){
     logBiz.info('readFileWraps is null');  
     return;
   }
+
+  //读取文件信息
   let wraps=yield readFileWraps;
 
   //去掉不需要读取的文件
   let handleWraps=wraps.filter(function(wrap){
+      logIndex.info(JSON.stringify(wrap));
+
+      //全局变量，记录上一次处理的位置
+      paths[wrap.index].offset=wrap.offset;
+
       if (wrap.readBufSize==0){
         logBiz.info('file no change or file has rename');
         return false;
@@ -71,15 +59,16 @@ function* bizCode(){
 
   //读取新增文件字符串
   let readStrWraps=handleWraps.map(function(wrap){
-    //全局变量，记录上一次处理的位置
-    paths[wrap.index].offset=wrap.offset;
 
     return readStrWrap(wrap);
   });
 
   //处理文件格式
   let fileStrs=yield readStrWraps;
-
+  if (fileStrs.length==0){
+    logBiz.info('no data');
+    return;
+  }
   console.log(fileStrs);
   //发送数据
   sendData(fileStrs);
@@ -93,9 +82,20 @@ function onError(err){
   logIndex.error("error:"+err.stack);
 }
 
-//执行
-doBiz();
+/*
+ * 代码执行入口
+ */
+function main(){
+  //没有配置文件，程序结束
+  if (!paths||paths.length==0){
+    logIndex.info('paths is null.')
+    process.exit(0);
+  }
+  
+  doBiz();
+}
 
+main();
 /*
  *定期处理
  */
