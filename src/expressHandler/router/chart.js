2e0ae4c3151e10d48c9e321f9595c55c1ead8a4e
txx3258 <soft_tangxiaoxian@163.com>
2016-02-19 16:24:19 +0800

    web express

---
+++ src/expressHandler/router/chart.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express=require('express');
+var router=express.Router();
+
+router.get('/chart',function(req,res,next){
+
+});
+
+module.exports=router;
---
+++ src/httpServer.js
@@ -0,0 +1,51 @@
+'use strict';
+var http=require('http');
+var express=require('express');
+var app=express();
+var path = require('path');
+var config=require('../config');
+var logSys=require('./myLog4js').logSys;
+
+var chart=require('./expressHandler/router/chart');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//接口
+app.get('/',function(req,res){
+  res.send('hello world!!!');
+});
+
+app.get('/web',chart);
+
+//catch 404
+app.use(function(req,res,next){
+  var err=new Error('Not Found');
+  err.status=404;
+
+  next(err);
+});
+
+//catch 500
+app.use(function(err,req,res,next){
+  res.status(err.status||500);
+
+  res.render('error',{
+    message:err.message,
+    error:{}
+  });
+});
+
+//创建服务
+var server=http.createServer(app);
+
+app.set('port',8081);
+server.listen(8081);//config.WEB_PORT);
+server.on('error',function(){
+  logSys.warn('web express is error');
+});
+
+server.on('listening',function(){
+  logSys.info('web:express is listening');
+});
+
+module.exports=app;
