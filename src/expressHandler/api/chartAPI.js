3161a85574cc5eb9c0f64e5265a6ae1788091bb5
txx3258 <soft_tangxiaoxian@163.com>
2016-03-02 16:12:17 +0800

    mm

--- src/expressHandler/api/chartAPI.js
+++ src/expressHandler/api/chartAPI.js
@@ -14,7 +14,7 @@ function handleResult(req,res){
     //参数组装
     let query=req.query;
     let collectName=query.collectName;
-    let query=query.queryName.trim();
+    let date=query.queryName.trim();
     let si=query.si;
     let count=query.count;
 
@@ -23,8 +23,8 @@ function handleResult(req,res){
     var s=parseInt(si);
     si=isNaN(s)?0:s;
 
-    let sd=new Date(query+' 00:00:00').getTime();
-    let ed=new Date(query+' 23:59:59').getTime();
+    let sd=new Date(date+' 00:00:00').getTime();
+    let ed=new Date(date+' 23:59:59').getTime();
     let queryName={'d':{'$gte':sd,'$lt':ed}};
 
     //异步：连接mongo
