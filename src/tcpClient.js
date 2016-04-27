c5ebf31ef8095ce17304a2d71b5981df4fc894cf
txx3258 <tangxx3@LENOVO.COM>
2016-04-27 12:45:01 +0800

    未保存提交

--- src/tcpClient.js
+++ src/tcpClient.js
@@ -1,25 +1,25 @@
 'use strict';
 
-let co=require('co');
+let co = require('co');
 
-let config=require('../config.json');
-let myLog4js=require('./common/log');
-let logIndex=myLog4js.logIndex;
-let logBiz=myLog4js.logBiz;
-let logSys=myLog4js.logSys;
+let config = require('../config.json');
+let myLog4js = require('./common/log');
+let logIndex = myLog4js.logIndex;
+let logBiz = myLog4js.logBiz;
+let logSys = myLog4js.logSys;
 
 /*
  *引入异步代码处理块 
  */
-let readFileWrap=require('./fdHandler/readFileWrap');
-let readStrWrap=require('./fileHandler/readStrWrap');
-let sendData=require('./tcpHandler/tcpClient');
-let paths=config.PATHS;
+let readFileWrap = require('./fdHandler/readFileWrap');
+let readStrWrap = require('./fileHandler/readStrWrap');
+let sendData = require('./tcpHandler/tcpClient');
+let paths = config.PATHS;
 
 /*
  *业务处理
  */
-function doBiz(){
+function doBiz() {
   logIndex.info('doBiz is starting');
 
   //代码执行
@@ -28,47 +28,47 @@ function doBiz(){
   logIndex.info('doBiz is finishing');
 }
 
-function* bizCode(){
+function* bizCode() {
   //读取新增文件信息
-  let readFileWraps=paths.map(function(item,index){
-    logIndex.info('fd offset='+item.offset);
-    return readFileWrap(item,index);
+  let readFileWraps = paths.map(function (item, index) {
+    logIndex.info('fd offset=' + item.offset);
+    return readFileWrap(item, index);
   });
 
-  if (readFileWraps.length==0){
-    logBiz.info('readFileWraps is null');  
+  if (readFileWraps.length == 0) {
+    logBiz.info('readFileWraps is null');
     return;
   }
 
   //读取文件信息
-  let wraps=yield readFileWraps;
+  let wraps = yield readFileWraps;
 
   //去掉不需要读取的文件
-  let handleWraps=wraps.filter(function(wrap){
-      logIndex.info(JSON.stringify(wrap));
+  let handleWraps = wraps.filter(function (wrap) {
+    logIndex.info(JSON.stringify(wrap));
 
-      //全局变量，记录上一次处理的位置
-      paths[wrap.index].offset=wrap.offset;
+    //全局变量，记录上一次处理的位置
+    paths[wrap.index].offset = wrap.offset;
 
-      if (wrap.len==0){
-        logBiz.info('file no change or file has rename');
-        return false;
-      }
-      return true;
+    if (wrap.len == 0) {
+      logBiz.info('file no change or file has rename');
+      return false;
+    }
+    return true;
   });
 
   //读取新增文件字符串
-  let readStrWraps=handleWraps.map(function(wrap){
+  let readStrWraps = handleWraps.map(function (wrap) {
 
     return readStrWrap(wrap);
   });
 
   //处理文件格式
-  let fileStrs=yield readStrWraps;
+  let fileStrs = yield readStrWraps;
 
   //除去空文件内容
-  fileStrs.filter(function(item){
-    if (item){
+  fileStrs.filter(function (item) {
+    if (item) {
       return true;
     }
 
@@ -83,20 +83,20 @@ function* bizCode(){
 /*
  *错误处理
  */
-function onError(err){
-  logIndex.error("error:"+err.stack);
+function onError(err) {
+  logIndex.error("error:" + err.stack);
 }
 
 /*
  * 代码执行入口
  */
-function main(){
+function main() {
   //没有配置文件，程序结束
-  if (!paths||paths.length==0){
+  if (!paths || paths.length == 0) {
     logIndex.info('paths is null.');
     process.exit(0);
   }
-  
+
   doBiz();
 }
 
@@ -106,5 +106,5 @@ main();
 /*
  *定期处理
 */
-setInterval(doBiz,config.INTERVAL);
+setInterval(doBiz, config.INTERVAL);
 
